57d49076e9a5a2cc104dc3fd919bae78c7104086
Jiajie Chen <c@jia.je>
2021-01-10 14:27:56 +0800

    Initial winnowing algorithm

--- src/bin/cli.rs
+++ src/bin/cli.rs
@@ -1,8 +1,9 @@
+use adler32::RollingAdler32;
 use jieplag::{common::LineMatch, token::Token};
 use log::*;
 use regex::Regex;
 use std::{
-    collections::HashMap,
+    collections::{HashMap, VecDeque},
     fs::{read_dir, File},
     io::{Read, Write},
     path::{Path, PathBuf},
@@ -38,6 +39,78 @@ fn read_file_lines(s: &Path) -> anyhow::Result<Vec<String>> {
     let mut s = String::new();
     file.read_to_string(&mut s)?;
     Ok(s.lines().map(|l| String::from(l)).collect::<Vec<String>>())
+}
+
+pub struct Fingerprint {
+    pub hash: u32,
+    pub offset: usize,
+}
+
+fn fingerprint<I>(mut iter: I, noise: usize, guarantee: usize) -> Vec<Fingerprint>
+where
+    I: Iterator<Item = u8>,
+{
+    let mut res = vec![];
+    // initial rolling `noise`-gram hashes
+    let mut items = VecDeque::new();
+    let mut hasher = RollingAdler32::new();
+    for _ in 0..noise {
+        if let Some(e) = iter.next() {
+            items.push_back(e);
+            hasher.update(e);
+        } else {
+            // too short
+            return res;
+        }
+    }
+
+    // window of hashes
+    let window_size = guarantee - noise + 1;
+    let mut hashes = VecDeque::new();
+    for _ in 0..window_size {
+        hashes.push_back(u32::MAX);
+    }
+
+    let mut min_hash_index = 0;
+    let mut window_offset = 0;
+    while let Some(e) = iter.next() {
+        // update rolling hash
+        hasher.remove(noise, items.pop_front().unwrap());
+        items.push_back(e);
+        hasher.update(e);
+        let new_hash = hasher.hash();
+
+        if new_hash < hashes[min_hash_index] {
+            // a new minimum
+            min_hash_index = window_size - 1;
+            hashes.pop_front();
+            hashes.push_back(new_hash);
+            res.push(Fingerprint {
+                hash: new_hash,
+                offset: window_offset,
+            });
+        } else {
+            // update window
+            hashes.pop_front();
+            hashes.push_back(new_hash);
+            if min_hash_index == 0 {
+                for i in 0..window_size {
+                    if hashes[i] < hashes[min_hash_index] {
+                        min_hash_index = i;
+                    }
+                }
+                res.push(Fingerprint {
+                    hash: new_hash,
+                    offset: window_offset - window_size + 1 + min_hash_index,
+                });
+            } else {
+                min_hash_index -= 1;
+            }
+        }
+
+        window_offset += 1;
+    }
+    res
 }
 
 fn main() -> anyhow::Result<()> {
@@ -119,19 +192,56 @@ fn main() -> anyhow::Result<()> {
         }
 
         let template_token = &template_tokens[submission];
-        let template_token_kind: Vec<u8> = template_token.iter().map(|t| t.kind).collect();
+        let template_fingerprint = fingerprint(template_token.iter().map(|t| t.kind), 20, 40);
         let mut local_tokens = vec![];
+        let mut local_fingerprints = vec![];
+        let mut index: HashMap<u32, Vec<usize>> = HashMap::new();
         for i in 0..keys.len() {
-            let mut token = all_tokens[submission][keys[i]].clone();
-            let token_kind: Vec<u8> = token.iter().map(|t| t.kind).collect();
-            let mut matches = rkr_gst::run(&token_kind, &template_token_kind, 40, 20);
-            matches.sort_by_key(|m| m.pattern_index);
-            for m in matches.iter().rev() {
-                token.drain(m.pattern_index..m.pattern_index + m.length);
+            let token = all_tokens[submission][keys[i]].clone();
+            let fingerprint = fingerprint(token.iter().map(|t| t.kind), 20, 40);
+            // insert to index: fingerprint => id
+            for f in &fingerprint {
+                index.entry(f.hash).or_default().push(i);
             }
+            local_fingerprints.push(fingerprint);
             local_tokens.push(token);
         }
 
+        // exclude fingerprints in template
+        for f in &template_fingerprint {
+            index.remove(&f.hash);
+        }
+
+        // create two dimensional matrix
+        let mut m = vec![vec![0; keys.len()]; keys.len()];
+        for hash in index.keys() {
+            let v = &index[hash];
+            // add to matrix
+            for i in 0..v.len() {
+                for j in (i + 1)..v.len() {
+                    m[v[i]][v[j]] += 1;
+                    m[v[j]][v[i]] += 1;
+                }
+            }
+        }
+
+        // find large elements
+        for left in 0..keys.len() {
+            for right in (left + 1)..keys.len() {
+                let val = m[left][right];
+                if val > 1000 {
+                    // show info
+                    info!(
+                        "Possible plagarism: {} and {}: {} matches",
+                        keys[left].display(),
+                        keys[right].display(),
+                        val
+                    );
+                }
+            }
+        }
+
+        /*
         for left in 0..keys.len() {
             for right in (left + 1)..keys.len() {
                 let token_left = &local_tokens[left];
@@ -215,6 +325,7 @@ fn main() -> anyhow::Result<()> {
                 }
             }
         }
+        */
     }
     Ok(())
 }
--- src/common.rs
+++ src/common.rs
@@ -3,12 +3,46 @@ use log::*;
 use std::fmt::Display;
 use uuid::Uuid;
 
+use crate::token::Token;
+
 #[derive(Copy, Clone)]
 pub struct LineMatch {
     pub left_from: u32,
     pub left_to: u32,
     pub right_from: u32,
     pub right_to: u32,
+}
+
+pub fn find_matches(left: &[Token], right: &[Token]) -> Vec<LineMatch> {
+    let mut line_matches = vec![];
+    let left_kind: Vec<u8> = left.iter().map(|t| t.kind).collect();
+    let right_kind: Vec<u8> = right.iter().map(|t| t.kind).collect();
+    let mut matches = rkr_gst::run(&left_kind, &right_kind, 40, 20);
+    matches.sort_by_key(|m| m.pattern_index);
+
+    for m in &matches {
+        line_matches.push(LineMatch {
+            left_from: left[m.pattern_index].line,
+            left_to: left[m.pattern_index + m.length - 1].line,
+            right_from: right[m.text_index].line,
+            right_to: right[m.text_index + m.length - 1].line,
+        });
+    }
+
+    // merge consecutive matches in line
+    let mut i = 0;
+    while i + 1 < line_matches.len() {
+        if line_matches[i].left_to == line_matches[i + 1].left_from
+            && line_matches[i].right_to == line_matches[i + 1].right_from
+        {
+            line_matches[i].left_to = line_matches[i + 1].left_to;
+            line_matches[i].right_to = line_matches[i + 1].right_to;
+            line_matches.drain(i + 1..i + 2);
+        } else {
+            i = i + 1;
+        }
+    }
+    line_matches
 }
 
 pub fn generate_uuid() -> String {
