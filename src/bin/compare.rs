07f148eccb7dec32515623c15c85e2a09e62d3a6
Jiajie Chen <c@jia.je>
2022-09-17 19:45:09 +0800

    Consider template in compare.rs

--- src/bin/compare.rs
+++ src/bin/compare.rs
@@ -1,3 +1,4 @@
+use rkr_gst::Match;
 use std::{
     fs::File,
     io::Read,
@@ -12,8 +13,12 @@ struct Args {
     left: PathBuf,
 
     /// Path to right source
-    #[structopt(short = "T", long)]
+    #[structopt(short, long)]
     right: PathBuf,
+
+    /// Path to template source
+    #[structopt(short, long)]
+    template: Option<PathBuf>,
 }
 
 fn read_file_lines(s: &Path) -> anyhow::Result<Vec<String>> {
@@ -28,19 +33,70 @@ fn main() -> anyhow::Result<()> {
     env_logger::init();
 
     let token_left = jieplag::lang::tokenize(&opts.left).unwrap();
-    let token_left_kind: Vec<u8> = token_left.iter().map(|t| t.kind).collect();
+    let token_kind_left: Vec<u8> = token_left.iter().map(|t| t.kind).collect();
     let lines_left = read_file_lines(&opts.left)?;
+
     let token_right = jieplag::lang::tokenize(&opts.right).unwrap();
-    let token_right_kind: Vec<u8> = token_right.iter().map(|t| t.kind).collect();
+    let token_kind_right: Vec<u8> = token_right.iter().map(|t| t.kind).collect();
     let lines_right = read_file_lines(&opts.right)?;
 
-    let matches = rkr_gst::run(&token_left_kind, &token_right_kind, 40, 20);
+    let token_template = opts
+        .template
+        .as_ref()
+        .map(|p| jieplag::lang::tokenize(&p).unwrap());
+    let token_kind_template: Option<Vec<u8>> =
+        token_template.map(|t| t.iter().map(|t| t.kind).collect());
+
+    let initial_search_length = 40;
+    let minimum_match_length = 20;
+    let matches = rkr_gst::run(
+        &token_kind_left,
+        &token_kind_right,
+        initial_search_length,
+        minimum_match_length,
+    );
+
+    let left_template_matches = token_kind_template.as_ref().map(|t| {
+        rkr_gst::run(
+            &token_kind_left,
+            t,
+            initial_search_length,
+            minimum_match_length,
+        )
+    });
+    let right_template_matches = token_kind_template.as_ref().map(|t| {
+        rkr_gst::run(
+            &token_kind_left,
+            t,
+            initial_search_length,
+            minimum_match_length,
+        )
+    });
 
     for (idx, m) in matches.iter().enumerate() {
+        // skip if covered
+        if let Some(true) = left_template_matches.as_ref().map(|mm| {
+            mm.iter().any(|mmm: &Match| {
+                mmm.pattern_index <= m.pattern_index + m.length - 1
+                    && mmm.pattern_index + mmm.length - 1 >= m.pattern_index
+            })
+        }) {
+            continue;
+        }
+        if let Some(true) = right_template_matches.as_ref().map(|mm| {
+            mm.iter().any(|mmm: &Match| {
+                mmm.pattern_index <= m.text_index + m.length - 1
+                    && mmm.pattern_index + mmm.length - 1 >= m.text_index
+            })
+        }) {
+            continue;
+        }
+
         let line_from_left = token_left[m.pattern_index].line as usize - 1;
         let line_to_left = token_left[m.pattern_index + m.length - 1].line as usize - 1;
         let line_from_right = token_right[m.text_index].line as usize - 1;
         let line_to_right = token_right[m.text_index + m.length - 1].line as usize - 1;
+
         println!("Match #{}:", idx + 1);
         println!("Left:");
         println!("{}", lines_left[line_from_left..=line_to_left].join("\n"));
