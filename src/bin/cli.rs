6107fe6c2c35688d354d7ac91602c2f1bc356dfb
Jiajie Chen <c@jia.je>
2022-09-17 21:32:45 +0800

    Cleanup code

--- src/bin/cli.rs
+++ src/bin/cli.rs
@@ -4,10 +4,9 @@ use log::*;
 use regex::Regex;
 use std::{
     collections::{HashMap, VecDeque},
-    fs::{read_dir, File},
+    fs::read_dir,
     hash::{Hash, Hasher},
-    io::Read,
-    path::{Path, PathBuf},
+    path::PathBuf,
 };
 use structopt::StructOpt;
 use walkdir::WalkDir;
@@ -22,24 +21,9 @@ struct Args {
     #[structopt(short = "T", long)]
     template_directory: PathBuf,
 
-    /// Path to result directory
-    #[structopt(short, long)]
-    result_directory: PathBuf,
-
     /// Regex patterns for files to include
     #[structopt(short, long)]
     include: Vec<Regex>,
-
-    /// Threshold of similarity
-    #[structopt(short, long, default_value = "0.6")]
-    threshold: f32,
-}
-
-fn read_file_lines(s: &Path) -> anyhow::Result<Vec<String>> {
-    let mut file = File::open(s)?;
-    let mut s = String::new();
-    file.read_to_string(&mut s)?;
-    Ok(s.lines().map(|l| String::from(l)).collect::<Vec<String>>())
 }
 
 #[derive(Clone, Copy)]
@@ -303,7 +287,7 @@ fn main() -> anyhow::Result<()> {
         }
 
         let mut sorted_m: Vec<_> = m.iter().enumerate().collect();
-        sorted_m.sort_by_key(|(i, val)| **val);
+        sorted_m.sort_by_key(|(_, val)| **val);
         for (i, matches) in sorted_m.iter().rev().take(40) {
             let left = i % keys.len();
             let right = i / keys.len();
@@ -311,8 +295,6 @@ fn main() -> anyhow::Result<()> {
                 // skip duplicatie
                 continue;
             }
-            let token_left = &local_tokens[left];
-            let token_right = &local_tokens[right];
             let matches = **matches;
             // show info
             info!(
@@ -322,94 +304,6 @@ fn main() -> anyhow::Result<()> {
                 matches,
             );
         }
-
-        // find large elements
-
-        /*
-        for left in 0..keys.len() {
-            for right in (left + 1)..keys.len() {
-                let token_left = &local_tokens[left];
-                let token_left_kind: Vec<u8> = token_left.iter().map(|t| t.kind).collect();
-                let token_right = &local_tokens[right];
-                let token_right_kind: Vec<u8> = token_right.iter().map(|t| t.kind).collect();
-                // too similar to template
-                if token_left.len() < 1000 || token_right.len() < 1000 {
-                    continue;
-                }
-
-                let mut matches = rkr_gst::run(&token_left_kind, &token_right_kind, 40, 20);
-
-                let match_count: usize = matches.iter().map(|m| m.length).sum();
-
-                let ratio_left = match_count as f32 / token_left.len() as f32;
-                let ratio_right = match_count as f32 / token_right.len() as f32;
-                if ratio_left > opts.threshold && ratio_right > opts.threshold {
-                    // convert token matches to line matches
-                    let mut line_matches = vec![];
-                    for m in &matches {
-                        line_matches.push(LineMatch {
-                            left_from: token_left[m.pattern_index].line,
-                            left_to: token_left[m.pattern_index + m.length - 1].line,
-                            right_from: token_right[m.text_index].line,
-                            right_to: token_right[m.text_index + m.length - 1].line,
-                        });
-                    }
-
-                    // merge consecutive matches in line
-                    let mut i = 0;
-                    while i + 1 < line_matches.len() {
-                        if line_matches[i].left_to == line_matches[i + 1].left_from
-                            && line_matches[i].right_to == line_matches[i + 1].right_from
-                        {
-                            line_matches[i].left_to = line_matches[i + 1].left_to;
-                            line_matches[i].right_to = line_matches[i + 1].right_to;
-                            line_matches.drain(i + 1..i + 2);
-                        } else {
-                            i = i + 1;
-                        }
-                    }
-                    let left_lines: Vec<String> = read_file_lines(&keys[left].join(&submission))?;
-                    let right_lines: Vec<String> = read_file_lines(&keys[right].join(&submission))?;
-
-                    // show info
-                    info!(
-                        "Possible plagarism: {} and {}: left {} {} right {} {}",
-                        keys[left].display(),
-                        keys[right].display(),
-                        ratio_left,
-                        token_left.len(),
-                        ratio_right,
-                        token_right.len(),
-                    );
-
-                    let match_file_name = opts.result_directory.join(format!("match{}", num_match));
-                    let mut match_file = File::create(match_file_name)?;
-                    writeln!(
-                        &mut match_file,
-                        "Between {} and {}: {}",
-                        keys[left].display(),
-                        keys[right].display(),
-                        submission.display(),
-                    )?;
-                    matches.sort_by_key(|m| m.pattern_index);
-                    for m in &line_matches {
-                        writeln!(
-                            &mut match_file,
-                            "Left L{}-L{} match Right L{}-L{}:\n{}\n-----------------------------\n{}",
-                            m.left_from,
-                            m.left_to,
-                            m.right_from,
-                            m.right_to,
-                            left_lines[m.left_from as usize..m.left_to as usize].join("\n"),
-                            right_lines[m.right_from as usize..m.right_to as usize].join("\n"),
-                        )?;
-                    }
-
-                    num_match += 1;
-                }
-            }
-        }
-        */
     }
     Ok(())
 }
