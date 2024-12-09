d9c9920c96bbefeb31d71b658bf802ea66c6a102
Maciej Matraszek <matraszek.maciej@gmail.com>
2024-12-09 21:57:03 +0100

    core: add per-language feature flags
    This makes the core library lighter to build and to compile without libclang.so.

--- core/src/lang/tokenizer/mod.rs
+++ core/src/lang/tokenizer/mod.rs
@@ -1,7 +1,14 @@
+#[cfg(feature="cpp")]
 pub mod cpp;
+#[cfg(feature="javascript")]
 pub mod javascript;
+#[cfg(feature="lua")]
 pub mod lua;
+#[cfg(feature="python")]
 pub mod python;
+#[cfg(feature="rust")]
 pub mod rust;
+#[cfg(feature="sql")]
 pub mod sql;
+#[cfg(feature="verilog")]
 pub mod verilog;
